c7bfc6f0907bfe6984d53ae7a7903a998f9ca0d7
aleksandrawlazlo <aleksandra.wlazlo97@gmail.com>
2023-09-22 19:40:05 +0200

    Book entity created.

---
+++ src/main/java/com/libraryproject/librarysystem/book/Book.java
@@ -0,0 +1,26 @@
+package com.libraryproject.librarysystem.book;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookId;
+    private String title;
+    private Author bookAuthor;
+    private Type type;
+    private int numberOfPages;
+
+}
