3609e67f87d6ff0d9dcde9d18f72cff607e8625e
aleksandrawlazlo <aleksandra.wlazlo97@gmail.com>
2023-10-09 20:22:00 +0200

    Modified AuthorResponseDto - removed 2 elements:String biography and List<Book>.

--- src/main/java/com/libraryproject/librarysystem/dto/AuthorResponseDto.java
+++ src/main/java/com/libraryproject/librarysystem/dto/AuthorResponseDto.java
@@ -1,14 +1,8 @@
 package com.libraryproject.librarysystem.dto;
 
-import com.libraryproject.librarysystem.entity.Book;
-
-import java.util.List;
-
 public record AuthorResponseDto(
         Long authorId,
         String firstName,
-        String lastName,
-        String biography,
-        List<Book> listOfBooks
+        String lastName
 ) {
 }
